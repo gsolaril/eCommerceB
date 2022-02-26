6fe4c7919c251bf6b5939a564a07e43def5401db
Coco <gsolaril@itba.edu.ar>
2022-02-26 04:50:56 -0300

    Revised models and added dummy database

--- app/models.py
+++ app/models.py
@@ -1,84 +1,119 @@
-from django.db import models
 from decimal import Decimal
+from django.db import models
 from django.core.validators import MinValueValidator
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User, AbstractBaseUser
 
 # Create your models here.
 
 price = [ MinValueValidator(Decimal("0.01")) ]
 zero = [ MinValueValidator(Decimal("0.0")) ]
 
-class Account(models.Model):
+class User(AbstractBaseUser):
 
-    ID = models.CharField(max_length = 255, primary_key = True)
-    name = models.CharField(max_length = 255)
-    surname = models.CharField(max_length = 255)
-    email = models.EmailField(unique = True)
-    phone = models.CharField(max_length = 255)
-    token = models.CharField(max_length = 255)
-    avatar = models.CharField(max_length = 255)
+    USERNAME_FIELD = "email"
+
+    user_id = models.PositiveIntegerField(primary_key = True)
+    email = models.EmailField(max_length = 64, unique = True)
+    name = models.CharField(max_length = 64)
+    surname = models.CharField(max_length = 64)
+    phone = models.CharField(max_length = 16)
+    avatar = models.CharField(max_length = 255, null = True)
     birth = models.DateField(null = True)
+    since = models.DateField()
+    is_superuser = models.BooleanField(default = False)
 
 class Address(models.Model):
 
-    account = models.ForeignKey(to = Account, on_delete = models.CASCADE)
-    pin = models.PositiveIntegerField()
+    address_id = models.PositiveIntegerField(primary_key = True)
+    user = models.ForeignKey(to = User, on_delete = models.CASCADE)
+    latitude = models.DecimalField(max_digits = 11, decimal_places = 8)
+    longitude = models.DecimalField(max_digits = 11, decimal_places = 8)
     address = models.CharField(max_length = 255, null = False)
     address2 = models.CharField(max_length = 255)
     city = models.CharField(max_length = 255)
-    latitude = models.DecimalField(max_digits = 11, decimal_places = 8)
-    longitude = models.DecimalField(max_digits = 10, decimal_places = 8)
+    pin = models.PositiveIntegerField()
 
 class Shop(models.Model):
 
+    shop_id = models.PositiveIntegerField(primary_key = True)
     name = models.CharField(max_length = 255)
     description = models.TextField()
     email = models.EmailField(unique = True)
     phone = models.CharField(max_length = 255)
-    opensAt = models.TimeField(default = "00:00")
-    closesAt = models.TimeField(default = "00:00")
+    opens_at = models.TimeField(default = "00:00")
+    closes_at = models.TimeField(default = "00:00")
     unavailable = models.BooleanField(default = False)
     pin = models.PositiveIntegerField()
     address = models.CharField(max_length = 255, null = False)
     address2 = models.CharField(max_length = 255)
     city = models.CharField(max_length = 255)
     latitude = models.DecimalField(max_digits = 11, decimal_places = 8)
-    longitude = models.DecimalField(max_digits = 10, decimal_places = 8)
-    deliveryCost = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
-    deliveryRange = models.PositiveIntegerField(default = 0, validators = zero)
+    longitude = models.DecimalField(max_digits = 11, decimal_places = 8)
+    delivery_cost = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
+    delivery_range = models.PositiveIntegerField(default = 0, validators = zero)
     image = models.CharField(max_length = 255)
 
 class Product(models.Model):
 
-    name = models.CharField(max_length = 255, primary_key = True)
+    product_id = models.PositiveIntegerField(primary_key = True)
+    name = models.CharField(max_length = 255)
     slug = models.SlugField(null = True)
     rating = models.DecimalField(max_digits = 3, decimal_places = 2, validators = zero, null = True)
-    last_update = models.DateTimeField(auto_now = True)
+    last_update = models.DateField(auto_now = True)
     description = models.TextField()
     images = models.TextField()
 
 class Model(models.Model):
 
+    model_id = models.PositiveIntegerField(primary_key = True)
     product = models.ForeignKey(to = Product, on_delete = models.CASCADE)
+    model = models.CharField(max_length = 32, null = True)
     qmin = models.PositiveIntegerField()
     qmax = models.PositiveIntegerField()
-    qstep = models.PositiveIntegerField()
-    quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits = 8, decimal_places = 2, validators = price)
-    discount = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits = 10,
+        decimal_places = 2, validators = price)
 
-class Delivery(Account):
+class Delivery(models.Model): # To be improved as user
 
-    since = models.DateField(auto_now = True)
-    requests = models.PositiveIntegerField()
+    delivery_id = models.PositiveIntegerField(primary_key = True)
+    name = models.CharField(max_length = 64)
+    surname = models.CharField(max_length = 64)
+    phone = models.CharField(max_length = 16)
+    tasks = models.PositiveIntegerField()
     miles = models.DecimalField(max_digits = 4, decimal_places = 1, validators = zero)
     rating = models.DecimalField(max_digits = 3, decimal_places = 2, validators = zero)
+    since = models.DateField()
     shops = models.JSONField()
+
+class Order(models.Model):
+
+    STATUS = ["pending", "paid", "sent", "done", "error"]
+    STATUS_DEF = STATUS[0]
+    STATUS = list(zip(STATUS, STATUS))
+
+    order_id = models.PositiveIntegerField(primary_key = True)
+    when = models.DateTimeField(auto_now = True)
+    status = models.CharField(choices = STATUS, default = STATUS_DEF, max_length = 8)
+    user = models.ForeignKey(to = User, on_delete = models.SET_NULL, null = True)
+    gross = models.DecimalField(max_digits = 10, decimal_places = 2, validators = price, default = 0)
+    coupons = models.JSONField()
+    tax = models.DecimalField(max_digits = 10, decimal_places = 2, validators = price, default = 0)
+    discount = models.DecimalField(max_digits = 10, decimal_places = 2, validators = price, default = 0)
+    deli_cost = models.DecimalField(max_digits = 10, decimal_places = 2, validators = price, default = 0)
+    amount = models.DecimalField(max_digits = 10, decimal_places = 2, validators = price, default = 0)
+    reviewed = models.BooleanField(default = False)
+    cart = models.JSONField()
+    shop = models.ForeignKey(to = Shop, on_delete = models.SET_NULL, null = True)
+    address = models.ForeignKey(to = Address, on_delete = models.SET_NULL, null = True)
+    delivery = models.ForeignKey(to = Delivery, on_delete = models.SET_NULL, null = True)
 
 class Review(models.Model):
 
-    account = models.ForeignKey(to = Account, on_delete = models.SET_NULL, null = True)
+    review_id = models.PositiveIntegerField(primary_key = True)
+    user = models.ForeignKey(to = User, on_delete = models.SET_NULL, null = True)
+    order = models.ForeignKey(to = Order, on_delete = models.SET_NULL, null = True)
     content_type = models.ForeignKey(ContentType, on_delete = models.CASCADE)
     content_object = GenericForeignKey()
     object_id = models.PositiveIntegerField()
@@ -86,39 +121,26 @@ class Review(models.Model):
     when = models.DateTimeField(auto_now = True)
     review = models.TextField()
 
-#class ProductReview(Review): product = models.ForeignKey(to = Product, on_delete = models.CASCADE)
-#class ShopReview(Review): shop = models.ForeignKey(to = Shop, on_delete = models.CASCADE)
-#class DeliReview(Review): delivery = models.ForeignKey(to = Delivery, on_delete = models.CASCADE)
-
-class Order(models.Model):
-
-    STATUS = { "pg": "pending", "pd": "paid", "st": "sent", "ok": "done", "er": "error" }
-
-    when = models.DateTimeField(auto_now = True)
-    status = models.CharField(choices = STATUS.items(), default = "pg", max_length = 8)
-    shop = models.ForeignKey(to = Shop, on_delete = models.SET_NULL, null = True)
-    address = models.ForeignKey(to = Address, on_delete = models.SET_NULL, null = True)
-    delivery = models.OneToOneField(to = Delivery, on_delete = models.SET_NULL, null = True)
-    gross = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
-    coupons = models.JSONField()
-    tax = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
-    discount = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
-    deliCost = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
-    amount = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
-    reviews = models.JSONField()
-    cart = models.JSONField()
-
 class Payment(models.Model):
 
-    PAYMENT_MODES = { "pe": "personal", "mp": "mercpago" }
+    PAYMENT_MODES = ["personal", "mercpago", "neteller", "skrill", "uala"]
+    PAYMENT_DEF = PAYMENT_MODES[0]
+    PAYMENT_MODES = list(zip(PAYMENT_MODES, PAYMENT_MODES))
 
-    order = models.OneToOneField(to = Order, on_delete = models.CASCADE, primary_key = True)
-    transaction = models.PositiveIntegerField()
-    paymentMode = models.CharField(choices = PAYMENT_MODES.items(), default = "pe", max_length = 8)
-    amount = models.DecimalField(max_digits = 6, decimal_places = 2, validators = price)
-    when = models.DateTimeField(auto_now = True)
+    payment_id = models.PositiveIntegerField(primary_key = True)
+    when = models.DateTimeField()
+    order = models.OneToOneField(to = Order, on_delete = models.SET_NULL, null = True)
+    payment_mode = models.CharField(choices = PAYMENT_MODES, default = PAYMENT_DEF, max_length = 8)
+    amount = models.DecimalField(max_digits = 10, decimal_places = 2, validators = price)
 
 class Coupon(models.Model):
 
+    LIMIT_BASES = ["times", "day", "week", "month"]
+    LIMIT_BASES_DEF = LIMIT_BASES[0]
+    LIMIT_BASES = list(zip(LIMIT_BASES, LIMIT_BASES))
+
+    coupon_id = models.PositiveIntegerField(primary_key = True, default = 123456789)
+    limit_uses = models.PositiveIntegerField(default = 1)
+    limit_basis = models.CharField(choices = LIMIT_BASES, default = LIMIT_BASES_DEF, max_length = 5)
     expires = models.DateField(null = True)
     discount = models.JSONField()
--- set/settings.py
+++ set/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = "set.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mosh1_storefront",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "74223xyzW*L",
     }
 }
 
@@ -116,7 +119,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -126,3 +128,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "app.User"
